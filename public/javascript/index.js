87de8ae1c6eeb848d028f7eb363c1e074233506e
James Daniel <professor4cob@outlook.com>
2021-09-16 11:44:27 -0700

    20 min into project

---
+++ public/javascript/index.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded", event => {
+    let featured = document.querySelector("featured");
+    let left = document.querySelector("left");
+    let right = document.querySelector("right");
+
+    let features = ["app", "blog", "shop", "note", "game", "grave"];
+
+    left.addEventListener("click", e => {
+        e.preventDefault()
+        if ()
+    })
+
+})
